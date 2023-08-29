231c2409cd2e2ba4c2a5e5066a265f058b3b4864
Shajee-j <Shajeejm@gmail.com>
2023-08-29 06:04:58 -0400

    more accurate folder naming

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
-require "./classes/game"
-require "./classes/players"
-require "./classes/questions"
+require "./component_classes/game"
+require "./component_classes/players"
+require "./component_classes/questions"
 
 new_game = Game.new
 new_game.start!
+
